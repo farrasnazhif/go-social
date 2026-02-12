c688a0151a261801399bebc5f24471d1488645f0
farrasnazhif <farrasnazhif@gmail.com>
2026-02-12 18:47:56 +0700

    feat: implementing repository pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/farrasnazhif/go-social/internal/env"
+	"github.com/farrasnazhif/go-social/internal/store"
 )
 
 func main() {
@@ -11,10 +12,14 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
+
 	log.Fatal(app.run(mux))
 }
