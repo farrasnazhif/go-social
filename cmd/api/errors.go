8f91cc2b34f253ecda1f273d611b11642f8608e3
farrasnazhif <farrasnazhif@gmail.com>
2026-03-05 15:51:30 +0700

    feat: error packages

---
+++ cmd/api/errors.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// we differentiate the error packages/messages for client and developer
+
+// for developer = use log
+// for client = write into the response
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Internal server error: %s, path: %s, error: %s", r.Method, r.URL, err)
+
+	writeJSONError(w, http.StatusInternalServerError, "The server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad request error: %s, path: %s, error: %s", r.Method, r.URL, err)
+
+	app.badRequestResponse(w, r, err)
+}
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad request error: %s, path: %s, error: %s", r.Method, r.URL, err)
+
+	writeJSONError(w, http.StatusNotFound, "Route not found")
+}
