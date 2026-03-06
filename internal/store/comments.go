a935e1b62281f6c895eddcea5e04d34a273e50ee
farrasnazhif <farrasnazhif@gmail.com>
2026-03-06 13:18:00 +0700

    feat: add comments relationship to post entity

---
+++ internal/store/comments.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+// model
+type Comment struct {
+	ID        int64  `json:"id"`
+	UserID    int64  `json:"user_id"`
+	PostID    int64  `json:"post_id"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	User      User   `json:"user"`
+}
+
+type CommentStore struct {
+	db *sql.DB
+}
+
+func (s *CommentStore) GetPostByID(ctx context.Context, postID int64) ([]Comment, error) {
+	query := `
+		SELECT c.id, c.user_id, c.post_id, c.content, c.created_at, users.username, users.id FROM comments c
+		JOIN users on users.id = c.user_id
+		WHERE c.post_id = $1
+		ORDER BY c.created_at DESC
+	`
+
+	rows, err := s.db.QueryContext(ctx, query, postID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	comments := []Comment{}
+	for rows.Next() {
+		var c Comment
+		c.User = User{}
+		err := rows.Scan(&c.ID, &c.UserID, &c.PostID, &c.Content, &c.CreatedAt, &c.User.Username, &c.User.ID)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+
+	return comments, nil
+}
