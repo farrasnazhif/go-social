891c5ea22a5875f798ae51620557fe316372be6b
farrasnazhif <farrasnazhif@gmail.com>
2026-03-31 19:26:44 +0700

    feat: user activation handler

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -71,6 +71,8 @@ func (app *application) mount() http.Handler {
 
 		// GET /api/v1/users/{userID}
 		r.Route("/users", func(r chi.Router) {
+			r.Put("/activate/{token}", app.activateUserHandler)
+
 			r.Route("/{userID}", func(r chi.Router) {
 				r.Use(app.usersContextMiddleware)
 
